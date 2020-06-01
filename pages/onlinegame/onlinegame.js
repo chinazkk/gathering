fd729d4119e1bccbc0e03dc56cda272e84eba7c6
SoreMelrose <qckong23@163.com>
2020-06-01 22:56:14 +0800

    6.1 页面改善

--- pages/onlinegame/onlinegame.js
+++ pages/onlinegame/onlinegame.js
@@ -1,62 +1,59 @@
 const app = getApp();
 var util = require("../../script/utils.js");
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-    keyword:'',
+    needflesh: true,
+    keyword: '',
     tag: ['排序', '参与人数', '最新发布'],
-    order: '0',//	 0-参与人数 1-最新发布
-    imgurl: app.globalData.imgurl,
-    currentpage:1,//当前页数
-    fleshlimit:'7'//每次刷新页数
+    order: '0', //	 0-参与人数 1-最新发布
+    type:'0',
+    currentpage: 1, //当前页数
+    fleshlimit: '7', //每次刷新页数
+    imgurl: app.globalData.imgurl
   },
   choose(e) {
     console.log(e.currentTarget.dataset.id)
     this.setData({
       order: e.currentTarget.dataset.id,
-      currentpage:1
+      currentpage: 1
     })
     this.flesh()
   },
-  getkeyword(e){
+  getkeyword(e) {
     this.setData({
-      keyword:e.detail.value
+      keyword: e.detail.value
     })
+    this.flesh()
   },
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
     var that = this
-    //小组活动
     let url = app.globalData.URL + '/group/activity/list';
     let data = {
-      order:that.data.order,
-      type: '0',
-      limit:that.data.fleshlimit,
+      order: that.data.order,
+      type: that.data.type,
+      limit: that.data.fleshlimit,
       page: that.data.currentpage
     }
     util.get(url, data).then(function (res) {
       that.setData({
         groupinfo: res.data.data,
-        currentpage:that.data.currentpage+1
+        currentpage: that.data.currentpage
       })
     })
   },
-  flesh(){
-    var that=this
+  //搜索刷新 或 选择刷新
+  flesh() {
+    var that = this
     let url = app.globalData.URL + '/group/activity/list';
     let data = {
-      order:that.data.order,
-      type: '0',
-      limit:that.data.fleshlimit,
+      keyword: that.data.keyword,
+      order: that.data.order,
+      type: that.data.type,
+      limit: that.data.fleshlimit,
       page: that.data.currentpage
     }
     util.get(url, data).then(function (res) {
-      console.log(res.data)
+      console.log('flesh', res.data)
       that.setData({
         groupinfo: res.data.data,
       })
@@ -66,30 +63,38 @@ Page({
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    console.log("上拉刷新")
-    var that=this
-    let url = app.globalData.URL + '/group/activity/list';
-    let data = {
-      order:that.data.order,
-      type: '0',
-      limit:that.data.fleshlimit,
-      page: that.data.currentpage
-    }
-    util.get(url, data).then(function (res) {
-      console.log('flesh',res.data)
-      let tmp=that.data.groupinfo
-      for(let i of res.data.data)
-      tmp.push(i)
-      that.setData({
-        groupinfo: tmp,
-        currentpage:that.data.currentpage+1
+    var that = this
+    if (that.data.needflesh) {
+      console.log("上拉刷新")
+      let url = app.globalData.URL + '/group/activity/list';
+      let data = {
+        order: that.data.order,
+        type: that.data.type,
+        limit: that.data.fleshlimit,
+        page: that.data.currentpage + 1
+      }
+      util.get(url, data).then(function (res) {
+        console.log('flesh', res.data)
+        if (!res.data.data.length) {
+          that.setData({
+            needflesh: false
+          })
+        } else {
+          let tmp = that.data.groupinfo
+          for (let i of res.data.data)
+            tmp.push(i)
+          that.setData({
+            groupinfo: tmp,
+            currentpage: that.data.currentpage + 1
+          })
+        }
       })
-    })
+    }
   },
-  todetail(e){
+  todetail(e) {
     console.log(e.currentTarget.dataset.id)
     wx.navigateTo({
-      url: '/pages/',
+      url: '/pages/activityDetail/activityDetail?id='+e.currentTarget.dataset.id,
     })
   }
 })
