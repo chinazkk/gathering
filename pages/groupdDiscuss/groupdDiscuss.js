326ed6becccf5dec3002001a10639366351b5e80
SoreMelrose <qckong23@163.com>
2020-07-10 22:32:47 +0800

    7.10

--- pages/groupdDiscuss/groupdDiscuss.js
+++ pages/groupdDiscuss/groupdDiscuss.js
@@ -5,10 +5,13 @@ Page({
   data: {
     thumb: 1,
     text: '',
-    test:[{
-      thumb:1
+    test: [{
+      thumb: 1
     }],
-    talkinfo: []
+    talkinfo: [],
+    showDel: true,
+    hiddenmodalput: true,
+    message: ''
   },
   InputFocus(e) {
     this.setData({
@@ -25,22 +28,102 @@ Page({
       text: e.detail.value
     })
   },
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  tothumb(e){
+
+  talkback: function (e) {
+    console.log(e.currentTarget.dataset.userid)
+    this.setData({
+      chooseindex: e.currentTarget.dataset.id,
+      visible: true
+    })
+
+    if (e.currentTarget.dataset.userid == this.data.userid) {
+      this.setData({
+        showDel: true
+      })
+    } else {
+      this.setData({
+        showDel: false
+      })
+    }
+  },
+  getmessage(e) {
+    this.setData({
+      message: e.detail.value
+    })
+  },
+  //点击按钮弹窗指定的hiddenmodalput弹出框  
+  modalinput: function () {
+    this.setData({
+      hiddenmodalput: !this.data.hiddenmodalput,
+      visible: !this.data.visible
+    })
+  },
+  //取消按钮  
+  cancel: function () {
+    this.setData({
+      hiddenmodalput: true
+    });
+  },
+  //确认  
+  reply() {
     var that = this
-    let url = app.globalData.URL + '/group/topic/praise';
-    let t=e.currentTarget.dataset.index
+    let url = app.globalData.URL + '/group/topic';
     var data = {
-     id:e.currentTarget.dataset.id
+      content: that.data.message,
+      group_id: that.data.groupnum,
+      topic_id: that.data.chooseindex,
+      user_id: that.data.userid
     }
     util.post(url, data).then(function (res) {
       console.log(res.data)
-      let tmp='talkinfo['+t+'].praise'
       if (res.data.code == 200) {
         that.setData({
-          [tmp]:true
+          message: '',
+          text: '',
+          hiddenmodalput: true
+        })
+        wx.showToast({
+          title: '提交成功',
+          duration: 2000,
+          success: function () {}
+        })
+        that.secondload()
+      } else {
+        wx.showToast({
+          title: '提交失败',
+          image: '/img/fail.png',
+          icon: 'success',
+          duration: 2000
+        })
+      }
+    }).catch(function (res) {
+      console.log(res)
+      wx.showToast({
+        title: '提交失败！',
+        icon: 'success',
+        duration: 2000
+      })
+    })
+  },
+  handleCancel: function () {
+    this.setData({
+      visible: false
+    });
+  },
+  tothumb(e) {
+    var that = this
+    let url = app.globalData.URL + '/group/topic/praise';
+    let t = e.currentTarget.dataset.index
+    var data = {
+      id: e.currentTarget.dataset.id
+    }
+    util.post(url, data).then(function (res) {
+      console.log(res.data)
+      let tmp = 'talkinfo[' + t + '].praise'
+      if (res.data.code == 200) {
+        that.secondload()
+        that.setData({
+          [tmp]: true
         })
         wx.showToast({
           title: '点赞成功',
@@ -70,26 +153,29 @@ Page({
       title: '加载中...',
       mask: true //显示触摸蒙层  防止事件穿透触发
     });
+
     console.log(options.id)
     wx.setStorageSync('groupid', options.id)
     this.setData({
       groupnum: options.id
     })
     let url = app.globalData.URL + '/group/topic/list';
-    let url2 = app.globalData.URL + '/group/topic/praise';
     let data = {
       group_id: options.id,
       limit: '10',
       page: '1'
     }
-    
+
     util.get(url, data).then(function (res) {
       let tmp = res.data.data
       for (let i of tmp) {
         i.lasttime = time.formatMsgTime(i.create_time)
+        for (let j of i.reply)
+          j.lasttime = time.formatMsgTime(j.create_time)
       }
       that.setData({
         talkinfo: tmp,
+        userid: wx.getStorageSync('userId')
       })
       wx.hideLoading()
     })
@@ -106,12 +192,17 @@ Page({
       let tmp = res.data.data
       for (let i of tmp) {
         i.lasttime = time.formatMsgTime(i.create_time)
+        for (let j of i.reply)
+          {
+            j.lasttime = time.formatMsgTime(j.create_time)
+          }
       }
       that.setData({
         talkinfo: tmp,
       })
     })
   },
+  //普通评论 不是评论回复
   commit(e) {
     var that = this
     let url = app.globalData.URL + '/group/topic';
@@ -124,7 +215,7 @@ Page({
       console.log(res.data)
       if (res.data.code == 200) {
         that.setData({
-          text:''
+          text: ''
         })
         wx.showToast({
           title: '提交成功',
