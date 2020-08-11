937cc7bee6d9332395e5611af04949e0cff6efaf
SoreMelrose <qckong23@163.com>
2020-08-11 14:37:59 +0800

    bug fixed

--- pages/userTalk2/userTalk2.js
+++ pages/userTalk2/userTalk2.js
@@ -9,7 +9,7 @@ Page({
   data: {
     messageinfo: [],
     currentpage: 1, //当前页数
-    fleshlimit: '20', //每次刷新页数
+    fleshlimit: '5', //每次刷新页数
     scrollHeight: '100vh',
     inputBottom: 0,
     input: ''
@@ -34,6 +34,7 @@ Page({
       InputBottom: 0
     })
   },
+  //提交评论
   submit() {
     var that = this
     let url = app.globalData.URL + '/inform/message';
@@ -49,91 +50,64 @@ Page({
           title: '发送成功',
           duration: 2000,
         })
-        that.setData({
-          input: ''
-        })
-        that.secondload()
-      }
-    })
-  },
-  secondload() {
-    var that = this
-    wx.showLoading({
-      title: '加载中...',
-      mask: true //显示触摸蒙层  防止事件穿透触发
-    });
-    // initData(this);
-    this.setData({
-      cusHeadIcon: wx.getStorageSync('userInfo').avatarUrl,
-      userid: wx.getStorageSync('userId'),
-      talkid: that.data.talkid
-    });
-    //对话者的信息
-    let url = app.globalData.URL + '/user';
-    var data = {
-      user_id: that.data.talkid,
-    }
-    util.get(url, data).then(function (res) {
-      that.setData({
-        anotherUser: res.data.data
-      })
-    })
-    //自己的身份信息
-    url = app.globalData.URL + '/user';
-    data = {
-      user_id: wx.getStorageSync('userId'),
-    }
-    util.get(url, data).then(function (res) {
-      that.setData({
-        userInfo: res.data.data
-      })
-    })
-    //消息列表
-    url = app.globalData.URL + '/inform/message/list';
-    data = {
-      page: that.data.currentpage,
-      limit: that.data.fleshlimit,
-      id: that.data.talkid,
-    }
-    util.get(url, data).then(function (res) {
-      console.log('message list', res.data)
-      let test = res.data.data
-      test.forEach((item) => {
-        //这里需要截取的内容
-        item.create_time = item.create_time.substring(5, 16)
-      })
-      console.log(test.length)
-      //按時間排序
-      for (var i = 0; i < test.length; i++) {
-        for (var u = i + 1; u < test.length; u++) {
-          if (test[i].create_time > test[u].create_time) {
-            // console.log(test[i].create_time)
-            var num = [];
-            num = test[i];
-            test[i] = test[u];
-            test[u] = num;
-          }
-        }
-      }
-      for (var i = 0; i < test.length; i++) {
-          var repTime = test[i].create_time.replace(/-/g, '/')
-          test[i].formaltime = Date.parse(repTime) / 1000
-      }
 
-      that.setData({
-        messageinfo: test
-      })
-      wx.hideLoading()
-    })
+        var tmp={}
+        tmp.status=true
+        tmp.content=that.data.input
+        tmp.to_id=that.data.anotherUser.id
+        tmp.from_id=that.data.userInfo.id
+        var timestamp = Date.parse(new Date());
+        timestamp = timestamp / 1000;
+        console.log("当前时间戳为：" + timestamp);
+        var n = timestamp * 1000;
+        var date = new Date(n);
+        var Y = date.getFullYear();
+        var M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1);
+        var D = date.getDate() < 10 ? '0' + date.getDate() : date.getDate();
+        var h = date.getHours();
+        var m = date.getMinutes();
+        // //秒  
+        // var s = date.getSeconds();
+        // console.log("当前时间：" + Y + M + D + h + ":" + m + ":" + s)
+        var nowtime = M + '-' + D+' '+h+":"+m
+        console.log(nowtime)
+        tmp.create_time=nowtime
+        tmp.formaltime=timestamp
+
+        var info=that.data.messageinfo
+        info.push(tmp)
+        that.setData({
+          input: '',
+          messageinfo:info
+        })
+      }
+    }).then( 
+      setTimeout(function () {
+        //滚动到底部
+        wx.pageScrollTo({
+          duration: 20,
+          selector:".bottoms"
+        })
+       }, 100) 
+      )
   },
+
   getinput(e) {
     this.setData({
       input: e.detail.value
     })
   },
-  sorttime(a, b) {
-    return a.create_time - b.create_time; //由低到高
-    //return b.distance - a.distance;//由高到低
+  //页面自动滚动到底部
+  pageScrollToBottom:function(){
+    wx.createSelectorQuery().select('cu-item').boundingClientRect(function(rect){
+        // console.log(rect.height);
+        wx.pageScrollTo({
+            scrollTop: rect.height,
+        });
+    }).exec()
+  },
+  flesg(){
+
   },
   onLoad: function (options) {
     console.log('userid', options.id)
@@ -184,17 +158,18 @@ Page({
       })
       console.log(test.length)
       //按時間排序
-      for (var i = 0; i < test.length; i++) {
-        for (var u = i + 1; u < test.length; u++) {
-          if (test[i].create_time > test[u].create_time) {
-            // console.log(test[i].create_time)
-            var num = [];
-            num = test[i];
-            test[i] = test[u];
-            test[u] = num;
-          }
-        }
-      }
+      // for (var i = 0; i < test.length; i++) {
+      //   for (var u = i + 1; u < test.length; u++) {
+      //     if (test[i].create_time > test[u].create_time) {
+      //       // console.log(test[i].create_time)
+      //       var num = [];
+      //       num = test[i];
+      //       test[i] = test[u];
+      //       test[u] = num;
+      //     }
+      //   }
+      // }
+      test=test.reverse()
       for (var i = 0; i < test.length; i++) {
           var repTime = test[i].create_time.replace(/-/g, '/')
           test[i].formaltime = Date.parse(repTime) / 1000
@@ -259,51 +234,49 @@ Page({
     wx.navigateBack({})
   },
   /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    console.log('onPullDownRefresh')
+    var that=this
+    //消息列表
+    var url = app.globalData.URL + '/inform/message/list';
+    var data = {
+      page: that.data.currentpage+1,
+      limit: that.data.fleshlimit,
+      id:this.data.talkid,
+    }
+    util.get(url, data).then(function (res) {
+      console.log('message list', res.data)
+      let info=that.data.messageinfo
+      let test = res.data.data
+      test=test.reverse()
+      test.forEach((item) => {
+        //这里需要截取的内容
+        item.create_time = item.create_time.substring(5, 16)
+      })
+      console.log(test.length)
+      test=test.reverse()
+      for (var i = 0; i < test.length; i++) {
+          var repTime = test[i].create_time.replace(/-/g, '/')
+          test[i].formaltime = Date.parse(repTime) / 1000
+          info.unshift(test[i])
+      }
+      that.setData({
+        messageinfo: info,
+        currentpage:that.data.currentpage+1
+        
+      })
+      wx.hideLoading()
+      wx.stopPullDownRefresh()
+    })
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
 
+  onReachBottom: function () {
   },
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
